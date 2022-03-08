ae3dcdd6f033831181265e7198e3bdc09219eb30
Michael-Aladejobi <94129364+Michael-Aladejobi@users.noreply.github.com>
2022-03-08 14:35:06 +0100

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,187 @@
+
+
+
+
+## control structures
+
+# num1 = int(input("enter a num: "))
+
+# if num1 > 10:
+
+#     print("This is over 10. ")
+
+# elif num1 == 10:
+
+#     print("This is equal to 10. ")
+
+# else:
+
+#     print("This is under 10. ")
+
+# num2 = int(input("enter another num: "))
+
+# if num2 >= 10:
+
+#     if num2 <= 20:
+
+#         print('Number is between 10 and 20. ')
+
+#     else:
+
+#         print('Number is over 20. ')
+
+# else:
+
+#     print('Number is under 10. ')
+
+# num = int(input("enter a num between 10 and 20: "))
+
+# if num >= 10 and num <= 20:
+
+#     print("Thanks.")
+
+# else:
+
+#     print("Out of bounds.")
+
+# num = int(input("enter an EVEN num between 1 and 5: "))
+
+# if num == 2 or num == 4:
+
+#     print("Thanks.")
+
+# else:
+
+#     print("Out of bounds.")
+
+# 012
+
+# numOne = int(input('enter a num: '))
+
+# numTwo = int(input('enter another num: '))
+
+# if numOne > numTwo:
+
+#     print(numTwo, numOne) # display number2 first
+
+# else:
+
+#     print(numOne, numTwo) # display number1 first
+
+# 013
+
+# numUnder20 = int(input("enter a number under 20: "))
+
+# if numUnder20 > 20:
+
+#     print("too high")
+
+# else:
+
+#     print("Thank you.")
+
+# 014
+
+# numBtwn10and20 = int(input("Enter a num between  10 and 20: "))
+
+# if numBtwn10and20 >= 10 and numBtwn10and20 <= 20:
+
+#     print("thank you")
+
+# else:
+
+#     print("incorrect message.")
+
+# 015
+
+# favColor = str(input("enter your fav color: "))
+
+# favColor = str.capitalize(favColor)
+
+# if favColor == "Red" or favColor == "red" or favColor == "RED":
+
+#     print('I like red too.')
+
+# else:
+
+#     print('I dont like ', favColor, ". I prefer red.")
+
+# 016
+
+# qtnrainy = str(input("is it raining? "))
+
+# qtnrainy = str.lower(qtnrainy)
+
+# if qtnrainy == "yes":
+
+#     qtnwindy = str(input("is it windy?: "))
+
+#     qtnwindy = str.lower(qtnwindy)
+
+#     if qtnwindy == "yes":
+
+#         print("too windy for an umbrella")
+
+#     else:
+
+#         print("take an umbrella")
+
+# else:
+
+#     print("enjoy your day")
+
+# 017
+
+# urAge = int(input("enter your age: "))
+
+# if urAge >= 18:
+
+#     print("You can vote.")
+
+# elif urAge == 17:
+
+#     print("You can learn to drive.")
+
+# elif urAge == 16:
+
+#     print("You can buy a lottery ticket.")
+
+# elif urAge < 16:
+
+#     print("You can go Trick-or-Treating.")
+
+# 018
+
+# number = int(input("enter a num: "))
+
+# if number < 10:
+
+#     print("too low.")
+
+# elif number >= 10 and number <= 20:
+
+#     print("correct.")
+
+# else:
+
+#     print("too high.")
+
+# # 019
+
+# onetwothree = int(input("enter 1, 2, or 3: "))
+
+# if onetwothree == 1:
+
+#     print("Thank you.")
+
+# elif onetwothree == 2:
+
+#     print("Well done.")
+
+# elif onetwothree == 3:
+
+#     print("Correct.")
+
+# else:
+
+#     print("Error message.")
