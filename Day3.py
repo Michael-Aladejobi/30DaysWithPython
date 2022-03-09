15540544a7a1a2d4ec9550f624c58733ea5f9de5
Michael-Aladejobi <94129364+Michael-Aladejobi@users.noreply.github.com>
2022-03-09 02:48:08 +0100

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,99 @@
+# strings
+
+# print("Mac's home has")
+
+# name = "Books"
+
+# num = 40
+
+# addNumAndName = str(num) + " " + name
+
+# print(addNumAndName, " in it.")
+
+# len(name)
+
+# 020
+
+# name = input("Enter your name: ")
+
+# print(len(name))
+
+# # 021
+
+# fname = input("Enter your first name: ")
+
+# surname = input("Enter your surname: ")
+
+# full_name = fname + " " + surname
+
+# print(full_name)
+
+# 022
+
+# fullName = input("Enter your full name name in lower case: ")
+
+# print(fullName.title())
+
+# 023
+
+# lineOfRhyme = input("Put first line of rhyme: ")
+
+# print(len(lineOfRhyme))
+
+# start_number = int(input("specify starting number: "))
+
+# end_number = int(input("specify ending number: "))
+
+# print(lineOfRhyme[start_number:end_number])
+
+# # 024
+
+# word = input("type a word in lower: ")
+
+# print(word.upper())
+
+# 025
+
+# fname = input("Enter first name: ")
+
+# if len(fname) < 5:
+
+#     sname = input("Enter surname: ")
+
+#     fullName = fname + sname
+
+#     print(fullName.upper())
+
+# elif len(fname) >= 5:
+
+#     print(fname.lower())
+
+# 026
+
+# Pig Latin takes the first consonant of a word,
+
+# moves it to the end of the word and adds on an
+
+# “ay”. If a word begins with a vowel you just add
+
+# “way” to the end. For example, pig becomes igpay,
+
+# banana becomes ananabay, and aadvark becomes
+
+# aadvarkway. Create a program that will ask the
+
+# user to enter a word and change it into Pig Latin.
+
+# Make sure the new word is displayed in lower case.
+
+# newWord = input("Enter a word: ")
+
+# newWord = newWord.lower()
+
+# if newWord[0] == "a" or newWord[0] == "e" or newWord[0] == "i" or newWord[0] == "o" or newWord[0] == "u":
+
+#     print(newWord[0:len(newWord)]+"w"+newWord[0]+"y")
+
+# else:
+
+#     print(newWord+"ay")
