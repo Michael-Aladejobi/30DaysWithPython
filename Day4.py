6763ddd70caca855357400296244f8b27a150069
Michael-Aladejobi <94129364+Michael-Aladejobi@users.noreply.github.com>
2022-03-10 00:26:32 +0100

    Create Day4.py

---
+++ Day4.py
@@ -0,0 +1,103 @@
+# math
+
+from ast import Num
+
+import math
+
+# ** = to power of
+
+# math.sqrt() math.pi round(num, d.p.) 
+
+# x // y is floor division
+
+# x % y is modulus of division
+
+# 027
+
+# num = float(input("Enter a num with lots of decimal places: "))
+
+# mxlyBy2 = num * 2
+
+# print(mxlyBy2)
+
+# 028
+
+# num = float(input("Enter a num with lots of decimal places: "))
+
+# mxlyBy2 = num * 2
+
+# print(round(mxlyBy2, 2))
+
+# 029
+
+# numOver500 = int(input("enter an int over 500: "))
+
+# findSqrt = math.sqrt(numOver500)
+
+# print(round(findSqrt, 2))
+
+# print(23.77 * 23.77) # test
+
+# 030
+
+# print(round(math.pi, 5))
+
+# 031
+
+# radius = int(input("enter a circle radius: "))
+
+# area = math.pi * (radius**2)
+
+# print(area)
+
+# print(3.14159 * 7 * 7) # test
+
+# 032
+
+# radius = int(input("enter the radius of a cylinder: "))
+
+# depth = int(input("enter the depth of a cylinder: "))
+
+# area = math.pi * (radius ** 2)
+
+# total_volume = area * depth
+
+# print(round(total_volume, 3))
+
+# 033
+
+# numOne = int(input("Enter a num: "))
+
+# numTwo = int(input("Enter num two: "))
+
+# divide = numOne//numTwo
+
+# division = math.floor(numOne/numTwo) # alternative
+
+# print(divide)
+
+# print(division) # print alternative
+
+# remain = numOne % numTwo
+
+# print("the remainder after division is: ", remain)
+
+# 034
+
+# print("1) Square \n2) Triangle\n")
+
+# num = int(input('enter a number: '))
+
+# if num == 1:
+
+#     side1 = int(input("enter the length of square's side: "))
+
+#     print("area is: ", side1*side1)
+
+# elif num == 2:
+
+#     base = int(input("enter the base of triangle: "))
+
+#     height = int(input("enter the height of triangle: "))
+
+#     print("area is ", 0.5*base*height)
