385e9a614e1ae7b4ade5811d35c889cfa64f62f1
Michael-Aladejobi <94129364+Michael-Aladejobi@users.noreply.github.com>
2022-03-14 10:20:04 +0100

    Create Day5.py

---
+++ Day5.py
@@ -0,0 +1,67 @@
+
+
+
+
+#import math
+
+# 035
+
+# diameter = int(input("enter a circle diameter: "))
+
+# radius = diameter/2
+
+# area = math.pi * (radius**2)
+
+# print(area)
+
+# print(3.14159 * 7 * 7) # test
+
+# 036
+
+# radius = int(input("enter the radius of a cylinder: "))
+
+# depth = int(input("enter the depth of a cylinder: "))
+
+# area = math.pi * (radius ** 2)
+
+# total_volume = area * depth
+
+# print(round(total_volume, 3))
+
+# 037
+
+# numOne = int(input("Enter a num: "))
+
+# numTwo = int(input("Enter num two: "))
+
+# divide = numOne//numTwo
+
+# division = math.floor(numOne/numTwo) # alternative
+
+# print(divide)
+
+# print(division) # print alternative
+
+# remain = numOne % numTwo
+
+# print("the remainder after division is: ", remain)
+
+# 038
+
+# print("1) Square \n2) Triangle\n")
+
+# num = int(input('enter a number: '))
+
+# if num == 1:
+
+#     side1 = int(input("enter the length of square's side: "))
+
+#     print("area is: ", side1*side1)
+
+# elif num == 2:
+
+#     base = int(input("enter the base of triangle: "))
+
+#     height = int(input("enter the height of triangle: "))
+
+#     print("area is ", 0.5*base*height)
