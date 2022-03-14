91995ebd04266b542b4e15c04188447ec8716a1b
Michael-Aladejobi <94129364+Michael-Aladejobi@users.noreply.github.com>
2022-03-14 10:22:12 +0100

    Create Day6.py

---
+++ Day6.py
@@ -0,0 +1,53 @@
+# urAge = int(input("enter your age: "))
+
+# if urAge >= 18:
+
+#     print("You can vote.")
+
+# elif urAge == 17:
+
+#     print("You can learn to drive.")
+
+# elif urAge == 16:
+
+#     print("You can buy a lottery ticket.")
+
+# elif urAge < 16:
+
+#     print("You can go Trick-or-Treating.")
+
+# 039
+
+# number = int(input("enter a num: "))
+
+# if number < 10:
+
+#     print("too low.")
+
+# elif number >= 10 and number <= 20:
+
+#     print("correct.")
+
+# else:
+
+#     print("too high.")
+
+# # 040
+
+# onetwothree = int(input("enter 1, 2, or 3: "))
+
+# if onetwothree == 1:
+
+#     print("Thank you.")
+
+# elif onetwothree == 2:
+
+#     print("Well done.")
+
+# elif onetwothree == 3:
+
+#     print("Correct.")
+
+# else:
+
+#     print("Error message.")
